bd9faa744a6e9f018d0ba842ee6913fd9237b333
김태현 <taehyk528@gmail.com>
2022-01-06 02:18:03 +0900

    Add: Template Project

---
+++ SkeletonProject/SkeletonProject/Presentation/Common/BaseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BaseViewController.swift
+//  SkeletonProject
+//
+//  Created by taehy.k on 2022/01/06.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        hierarchy()
+        layout()
+    }
+    
+    func style() {}
+    func hierarchy() {}
+    func layout() {}
+}
---
+++ SkeletonProject/SkeletonProject/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  SkeletonProject
+//
+//  Created by taehy.k on 2022/01/06.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func style() {
+        view.backgroundColor = .red
+    }
+}
