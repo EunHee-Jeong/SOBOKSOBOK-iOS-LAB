2d126331f7daf00948dd63679f2d45dc7d8b651d
seondal <sseondal@gmail.com>
2022-01-06 15:24:04 +0900

    Create ThirdVC

---
+++ xib-practice-seondal/xib-practice-seondal/SecondVC/ThirdVC.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdVC.swift
+//  xib-practice-seondal
+//
+//  Created by 김선오 on 2022/01/06.
+//
+
+import UIKit
+
+class ThirdVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
