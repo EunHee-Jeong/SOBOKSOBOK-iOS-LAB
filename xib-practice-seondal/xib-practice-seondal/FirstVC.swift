faa1c4a4024f0c4f1e0bc77c481c6ccdd4d16f3d
seondal <sseondal@gmail.com>
2022-01-07 14:07:40 +0900

    [Feat] FirstVC -> SecondVC

--- xib-practice-seondal/xib-practice-seondal/FirstVC.swift
+++ xib-practice-seondal/xib-practice-seondal/FirstVC.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class FirstVC: UIViewController {
 
+    @IBOutlet weak var MoveToSecondVC: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func MoveToSecondVC(_ sender: Any) {
+        let secondVC = SecondVC.instanceFromNib()
+        navigationController?.pushViewController(secondVC, animated: true)
     }
-    */
-
+    
 }
