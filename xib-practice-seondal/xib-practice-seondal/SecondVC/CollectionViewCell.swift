05fa693410ed623d384e60bae660becd9b528039
seondal <sseondal@gmail.com>
2022-01-07 14:03:44 +0900

    새로운방법 CollectionVIew

---
+++ xib-practice-seondal/xib-practice-seondal/SecondVC/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  xib-practice-seondal
+//
+//  Created by 김선오 on 2022/01/06.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- xib-practice-seondal/xib-practice-seondal/SecondVC/ThirdVC.swift
+++ xib-practice-seondal/xib-practice-seondal/SecondVC/ThirdVC.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class ThirdVC: UIViewController {
 
+    @IBOutlet weak var CollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        CollectionView.dataSource = self
+        CollectionView.delegate = self
+        CollectionView.register(CollectionViewCell.self)
     }
+}
 
+extension ThirdVC : UICollectionViewDelegate {
+    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ThirdVC : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: CollectionViewCell.self)
+        
+        return cell
+    }
+    
+    
 }
