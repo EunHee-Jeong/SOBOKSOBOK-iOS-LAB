ff6f06dd88a5136e3d5fd6e7a6de50dfd6848df6
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-07 19:09:27 +0900

    [Add] 연습용 repository 추가
    - EasyKit 패키지 추가
    - 폴더링 적용
    - 스토리보드 제거
    - 초기 scene: BaseViewController

---
+++ EunHee/EunHee/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  EunHee
+//
+//  Created by 정은희 on 2022/01/07.
+//
+
+import UIKit
+
+import EasyKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let vc = BaseViewController.instanceFromNib()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
+
