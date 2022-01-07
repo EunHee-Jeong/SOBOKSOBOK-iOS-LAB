5e451fe2609ad137e4bba85c0bf4bd38cc86d2ca
seondal <sseondal@gmail.com>
2022-01-07 18:32:55 +0900

    [Feat] UINavigationController 로 감싸기

--- xib-practice-seondal/xib-practice-seondal/SceneDelegate.swift
+++ xib-practice-seondal/xib-practice-seondal/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 window = UIWindow(windowScene: windowScene)
         let vc = FirstVC.instanceFromNib()
-        window?.rootViewController = vc
+        window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
     }
 
