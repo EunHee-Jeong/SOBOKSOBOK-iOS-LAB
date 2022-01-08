82f91fbf4cfc1f5db19a1c1d5f808a9f2bdabd9d
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-08 17:20:14 +0900

    [Add] 네트워킹 연습용 VC 생성
    - 아이폰13 mini
    - 로그인, 회원가입, 웰컴 화면

--- EunHee/EunHee/Application/SceneDelegate.swift
+++ EunHee/EunHee/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let vc = BaseViewController.instanceFromNib()
+        let vc = SignInViewController.instanceFromNib()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
